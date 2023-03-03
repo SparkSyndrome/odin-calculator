b3c9d154474ed012cb8799c32a8372e834196f3f
Christopher Burton <christopherburton1020@gmail.com>
2023-03-03 00:55:35 -0700

    Add switch to call functions by math operator

--- main.js
+++ main.js
@@ -17,3 +17,22 @@ function divide(num1, num2) {
   const quotient = num1 / num2;
   console.log(quotient);
 }
+
+function operate(operator, num1, num2) {
+  switch (operator) {
+    case "+":
+      add(num1, num2);
+      break;
+    case "-":
+      subtract(num1, num2);
+      break;
+    case "*":
+      multiply(num1, num2);
+      break;
+    case "/":
+      divide(num1, num2);
+      break
+    default:
+      console.log('Please enter a valid operator wrapped in quotes ( "+", "-", "*", or "/" )');
+  }
+}
