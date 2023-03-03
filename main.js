c287396d726f77241e4836746005918a9785851b
Christopher Burton <christopherburton1020@gmail.com>
2023-03-03 00:42:21 -0700

    Write basic math operator logic

---
+++ main.js
@@ -0,0 +1,19 @@
+function add(num1, num2) {
+  const sum = num1 + num2;
+  console.log(sum);
+}
+
+function subtract(num1, num2) {
+  const difference = num1 - num2;
+  console.log(difference);
+}
+
+function multiply(num1, num2) {
+  const product = num1 * num2;
+  console.log(product);
+}
+
+function divide(num1, num2) {
+  const quotient = num1 / num2;
+  console.log(quotient);
+}
