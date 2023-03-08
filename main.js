04422faa0edfd36dd8cb97b1b36e7facddc2406f
Christopher Burton <christopherburton1020@gmail.com>
2023-03-07 19:40:15 -0700

    Add basic calculator functionality

--- main.js
+++ main.js
@@ -1,50 +1,90 @@
+let operator = '';
+let lastNum = '';
+let currentNum = '';
+let result = '';
+
+document.addEventListener('DOMContentLoaded', function() {
+  const display = document.querySelector('.display');
+
+  const clear = document.querySelector('.clear');
+  const backspace = document.querySelector('.backspace');
+
+  const numbers = document.querySelectorAll('.number');
+  const operators = document.querySelectorAll('.operator');
+  const equals = document.querySelector('.equals');
+  
+  const decimal = document.querySelector('.decimal');
+  
+  numbers.forEach((number) => number.addEventListener('click', function(e) {
+    display.textContent = lastNum;
+    handleNumber(e.target.textContent);
+    display.textContent = currentNum;
+  }));
+
+  operators.forEach((operator) => operator.addEventListener('click', function(e) {
+    handleOperator(e.target.textContent);
+  }));
+
+  clear.addEventListener('click', function() {
+    operator = '';
+    lastNum = '';
+    currentNum = '';
+    display.textContent = currentNum;
+  })
+
+  equals.addEventListener('click', function() {
+    operate();
+    display.textContent = result;
+    lastNum = currentNum;
+    currentNum = '';
+  })
+})
+
+function handleNumber(num) {
+  if (currentNum.length < 10) {
+    currentNum += num;
+  }
+}
+
+function handleOperator(op) {
+  operator = op;
+  lastNum = currentNum;
+  currentNum = '';
+}
+
+function operate() {
+  lastNum = Number(lastNum);
+  currentNum = Number(currentNum);
+
+  if (operator === "+") {
+    add(lastNum, currentNum);
+  } else if (operator === "-") {
+    subtract(lastNum, currentNum);
+  } else if (operator === "×") {
+    multiply(lastNum, currentNum);
+  } else if (operator === "÷") {
+    divide(lastNum, currentNum);
+  } else {
+    return;
+  }
+}
+
 function add(num1, num2) {
   const sum = num1 + num2;
-  console.log(sum);
+  result = sum;
 }
 
 function subtract(num1, num2) {
   const difference = num1 - num2;
-  console.log(difference);
+  result = difference;
 }
 
 function multiply(num1, num2) {
   const product = num1 * num2;
-  console.log(product);
+  result = product;
 }
 
 function divide(num1, num2) {
   const quotient = num1 / num2;
-  console.log(quotient);
+  result = quotient;
 }
-
-function operate(operator, num1, num2) {
-  switch (operator) {
-    case "+":
-      add(num1, num2);
-      break;
-    case "-":
-      subtract(num1, num2);
-      break;
-    case "*":
-      multiply(num1, num2);
-      break;
-    case "/":
-      divide(num1, num2);
-      break
-    default:
-      console.log('Please enter a valid operator wrapped in quotes ( "+", "-", "*", or "/" )');
-  }
-}
-
-const display = document.querySelector('.display');
-const numbers = document.querySelectorAll('.number');
-let lastClicked;
-
-function populateDisplay(e) {
-  display.innerHTML = e.target.innerHTML;
-  lastClicked = e.target.innerHTML;
-  console.log(lastClicked);
-}
-
-numbers.forEach((number) => number.addEventListener('click', (e) => populateDisplay(e)))
